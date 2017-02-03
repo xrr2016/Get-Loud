a16209ec33fd950d7c32dc2c40ac5c370d5d6e18
xrr2016 <13286904669@163.com>
2017-02-03 22:53:36 +0800

    :hankey:

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 (() => {
     const map = new fabric.Canvas('map', {
-        backgroundColor: 'rgba(0,0,0,.2)',
-        selectionColor: 'blue',
+        backgroundColor: 'rgba(0,0,0,.1)',
+        selectionColor: '',
         selectionLineWidth: 2
     })
     const ball = new fabric.Circle({
-      radius:20,fill:'blue',left:100,top:120
+      radius:20,fill:'blue',left:100,top:480
     })
-    const basketball = new fabric.Image.fromURL('./img/basketball.png')
-    map.add(basketball)
-    map.add(ball)
+    const basket = new fabric.Ellipse({
+      angle:45,fill:'black',
+      height:50,width:100,
+      right:100,top:50,
+      color:'blue'
+    })
+    const title = new fabric.IText('hello world',{
+      left:100,top:200,color:'black'
+    })
+    // const basketball = new fabric.Image.fromURL('./img/basketball.png')
+    map.add(ball,basket,title)
 })()
