7921e1638e91b1da200942ad7578ed3895fdfaf6
xrr2016 <xiaoranran1993@163.com>
2017-05-22 22:44:07 +0800

    :heavy_plus_sign:

---
+++ client/src/components/app_header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppHeader = () => (
+  <Header>
+    Make Some Voices!
+  </Header>
+)
+
+export default AppHeader
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
-import './styles/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
