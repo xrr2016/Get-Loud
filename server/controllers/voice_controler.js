dc750d60602d09bdd6e8970fafb351fbfa905d91
xrr2016 <xiaoranran1993@163.com>
2017-05-31 22:16:28 +0800

    :sparkles:

--- server/controllers/voice_controler.js
+++ server/controllers/voice_controler.js
@@ -1,6 +1,23 @@
 import Voice from '../models'
 
-export const getAllVoices = async function () {
+// 获取全部voice
+export const getAllVoices = async () => {
    const voices = await Voice.findAll() 
    return voices
+}
+// 获取单个voice
+export const getVoice = async (_id) => {
+    const voice = await Voice.find({_id: _id})
+}
+// 新增一个voice
+export const createVoice = async () => {
+
+}
+// 更新一个voice
+export const updateVoice = async () => {
+
+}
+// 删除一个voice
+export const deleteVoice = async () => {
+
 }
